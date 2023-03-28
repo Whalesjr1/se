284034bef454dc97277c4a97d2169f027447dae9
spooney <124278317+spooney1998@users.noreply.github.com>
2023-03-28 16:17:45 +0100

    Add files via upload

---
+++ New-TEST-PROJECT/src/index.mjs
@@ -0,0 +1,65 @@
+/* Import dependencies */
+import express from "express";
+import mysql from "mysql2/promise";
+import DatabaseService from "./services/database.service.mjs";
+
+/* Create express instance */
+const app = express();
+const port = 3000;
+
+// Integrate Pug with Express
+app.set("view engine", "pug");
+
+// Serve assets from 'static' folder
+app.use(express.static("static"));
+
+const db = await DatabaseService.connect();
+const { conn } = db;
+
+/* Landing route */
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Sample API route
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+// Landing route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Gallery route
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+
+// About route
+app.get("/about", (req, res) => {
+  res.render("about", { title: "Boring about page" });
+});
+
+app.get("/cities", async (req, res) => {
+  const [rows, fields] = await db.getCities();
+  /* Render cities.pug with data passed as plain object */
+  return res.render("cities", { rows, fields });
+});
+
+app.get('/cities/:id', async (req, res) => {
+  const cityId = req.params.id;
+  const city = await db.getCity(cityId);
+  return res.render('city', { city });
+})
+
+// Returns JSON array of cities
+app.get("/api/cities", async (req, res) => {
+  const [rows, fields] = await db.getCities();
+  return res.send(rows);
+});
+
+// Run server!
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
